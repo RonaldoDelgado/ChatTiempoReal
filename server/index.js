3719c79df603e2a67db5f8d2e7ba5d67d1cad94c
RonaldoMonki <ronalldocorrea@gmail.com>
2023-02-24 10:44:34 -0500

    coneccion de socket entre fornt y back

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import morgan from "morgan";
+import { Server as SocketServer } from "socket.io";
+import http from "http";
+import { PORT } from "./config.js";
+import cors from "cors";
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+app.use(morgan("dev"));
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  console.log("usuario conectado");
+});
+
+server.listen(PORT);
+console.log("Server iniciado en el puerto " + PORT);
